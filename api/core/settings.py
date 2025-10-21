17f6be4df67f5a7c988fa6d53036a7c3d8a02565
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-10-21 14:27:30 -0300

    refactor: Adiciona configuração de CORS e atualiza o arquivo requirements.txt.

--- api/core/settings.py
+++ api/core/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +70,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+
+# CORS Configuration
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
