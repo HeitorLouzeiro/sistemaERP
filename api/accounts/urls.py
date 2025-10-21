dcdfb880f5001ceb645b539731a3d3bd974ed63c
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-10-21 16:07:51 -0300

    feat: Adiciona views e rotas para autenticação de usuários.

---
+++ api/accounts/urls.py
@@ -0,0 +1,12 @@
+# core/urls.py
+from accounts.views.signin import Signin
+from accounts.views.signup import Signup
+from accounts.views.user import GetUser
+from django.urls import path
+
+urlpatterns = [
+
+    path('signin/', Signin.as_view(), name='signin'),
+    path('signup/', Signup.as_view(), name='signup'),
+    path('user/', GetUser.as_view(), name='get_user'),
+]
