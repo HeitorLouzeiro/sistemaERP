7bbb6e260d4a15957c36b1e2cd384e830ef9de3e
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-11-07 10:48:41 -0300

    feat: adiciona rotas para gerenciamento de grupos, permissões e tarefas.

--- api/companies/urls.py
+++ api/companies/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 
-from companies.views.employees import Employees, EmployeeDetail
+from companies.views.employees import EmployeeDetail, Employees
+from companies.views.groups import GroupDetail, Groups
+from companies.views.permissions import PermissionDetail
+from companies.views.tasks import TaskDetail, Tasks
 
 urlpatterns = [
     path('employees/', Employees.as_view(), name='employees'),
-    path('employees/<int:employee_id>/', EmployeeDetail.as_view(), name='employee_detail'),
+    path('employees/<int:employee_id>/',
+         EmployeeDetail.as_view(), name='employee_detail'),
+
+    # Permissions
+    path('groups/', Groups.as_view(), name='groups'),
+    path('groups/<int:group_id>/',
+         GroupDetail.as_view(), name='group_detail'),
+    path('permissions/', PermissionDetail.as_view(), name='permission_detail'),
+
+    # Tasks
+    path('tasks/', Tasks.as_view(), name='tasks'),
+    path('tasks/<int:task_id>/',
+         TaskDetail.as_view(), name='task_detail'),
 ]
